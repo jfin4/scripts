71dcdccd4a940ac5b8683cd962879e9b1944aa3e
John Inman <john@johninman.dev>
2022-01-07 16:19:35 -0800

    updates

--- get-hours.r
+++ get-hours.r
@@ -8,6 +8,9 @@ data_today <- data[data$date == today, ]
 data_today$from <- as.POSIXlt(data_today$from, format = "%H%M")
 data_today$to <- as.POSIXlt(data_today$to, format = "%H%M")
 
+diffs_tot <- difftime(data_today$to, data_today$from, units = "hours")
+hours_tot <- as.numeric(sum(diffs_tot))
+
 data_today_misc <- data_today[data_today$task == "misc", ]
 diffs_misc <- difftime(data_today_misc$to, data_today_misc$from, units = "hours")
 hours_misc <- as.numeric(sum(diffs_misc))
@@ -15,7 +18,8 @@ hours_misc <- as.numeric(sum(diffs_misc))
 data_today_work <- data_today[data_today$task != "misc", ]
 diffs_work <- difftime(data_today_work$to, data_today_work$from, units = "hours")
 hours_work <- as.numeric(sum(diffs_work))
-hours <- c(hours_misc, hours_work)
-names(hours) <- c("Misc", "Worked")
+
+hours <- c(hours_misc, hours_work, hours_tot)
+names(hours) <- c("Misc", "Worked", "Total")
 print(hours)
 
