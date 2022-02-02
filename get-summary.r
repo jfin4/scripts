7ce746df88e01d983d996754308f8defb17a5130
John Inman <john@johninman.dev>
2022-02-01 18:01:18 -0800

    updates

--- get-summary.r
+++ get-summary.r
@@ -4,9 +4,7 @@ home <- 'C:/Users/JInman/msys/home/jfin/'
 file <- paste0(home, 'hours/', month, '.csv')
 data_all <- read.csv(file, strip.white=T, colClasses = "character")
 date <- format(Sys.time(), '%m%d')
-
 data_today <- data_all[data_all$date == date, ]
 data_work <- data_today[data_today$task != "misc", ]
-names(data_work) <- NULL
-out <- format(unique(data_work[4]), justify = "left")
-print(out, row.names = F)
+out <- unique(data_work[[4]])
+cat(paste("\t", out, "\n"))
