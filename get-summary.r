366310c0b79f123cb877b4a71e40a5911b94a020
John Inman <john@johninman.dev>
2022-02-09 17:53:29 -0800

    updates

--- get-summary.r
+++ get-summary.r
@@ -3,8 +3,10 @@ month <- format(Sys.time(), '%y-%m')
 home <- 'C:/Users/JInman/msys/home/jfin/'
 file <- paste0(home, 'hours/', month, '.csv')
 data_all <- read.csv(file, strip.white=T, colClasses = "character")
-date <- format(Sys.time(), '%m%d')
-data_today <- data_all[data_all$date == date, ]
-data_work <- data_today[data_today$task != "misc", ]
+arg <- commandArgs(trailingOnly = T)
+today <- format(Sys.time(), '%m%d')
+date <- ifelse(length(arg) == 1, arg, today) 
+data_date <- data_all[data_all$date == date, ]
+data_work <- data_date[data_date$task != "misc", ]
 out <- unique(data_work[[4]])
 cat(paste("\t", out, "\n"))
