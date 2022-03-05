01c5ac6c07b62df03aa1f259e1b145a93260d064
John Inman <john@johninman.dev>
2022-03-04 16:41:56 -0800

    updates

--- get-summary.r
+++ get-summary.r
@@ -6,7 +6,7 @@ home <- 'C:/Users/JInman/msys/home/jfin/'
 file <- paste0(home, 'hours/', month, '.csv')
 data_all <- read.csv(file, strip.white=T, colClasses = "character")
 today <- format(Sys.time(), '%m%d')
-date <- ifelse(length(arg) == 1, today, arg[2]) 
+date <- ifelse(args[2] == "", today, args[2]) 
 date <- today
 data_date <- data_all[data_all$date == date, ]
 data_work <- data_date[data_date$task != "misc", ]
